ee8da394f8e1fb2a44185822f98e53c544d98b67
AkarionBoi <39925253+AkarionBoi@users.noreply.github.com>
2020-05-26 14:49:15 +0100

    v1.0

---
+++ Fallout 4 Bingo/f4bingo/f4.js
@@ -0,0 +1,198 @@
+﻿var bingoList = [];
+bingoList[1] = [
+  { name: "Become addicted to a substance", types: ["items"] },
+  { name: "Collect 3 gold bars", types: ["items"] },
+  { name: "Enter any Power Armour", types: ["items"] },
+  { name: "Obtain the SPECIAL book", types: ["items"] },
+  { name: "Kill a Deathclaw", types: ["kill"] }
+];
+bingoList[2] = [
+  { name: "Obtain a Sea Captain's Hat", types: ["items"] },
+  { name: "Win a new car", types: ["items"] },
+  { name: "Get a HomeRun!", types: ["items"] },
+  { name: "Beat the 2nd level of Red Menace", types: ["star"] },
+  { name: "Kill someone by landing on them with power armour", types: ["kill"] },
+  { name: "Kill Kyle with a melee weapon", types: ["kill"] },
+  { name: "Get facial reconstruction surgery", types: ["meme"] }
+];
+bingoList[3] = [
+  { name: "Collect 4 Nuka Cola Quantums", types: ["items"] },
+  { name: "Collect 2 NukaCherrys", types: ["items"] },
+  { name: "Obtain the Cannibal perk and eat somebody", types: ["perk"] },
+  { name: "Construct a Bobblehead Stand", types: ["crafting"] },
+  { name: "Do the quest Out in Left Field", types: ["weapon"] },
+  { name: "Have at least 1000 caps", types: ["money"] },
+  { name: "Kill Travis", types: ["kill"] },
+  { name: "Obtain a pompadour wig", types: ["items"] }
+];
+bingoList[4] = [
+  { name: "Become addicted to 3 substances", types: ["items"] },
+  { name: "Obtain 5 fusion cores", types: ["items"] },
+  { name: "Raise a SPECIAL stat above 10", types: ["skill"] },
+  { name: "Discover at least 30 locations", types: ["location"] },
+  { name: "Brutally murder Finn", types: ["kill"] },
+  { name: "Score a basket", types: ["rep"] },
+  { name: "Recruit Cait", types: ["companion"] },
+  { name: "Kill the Vault Tec Rep", types: ["kill"] },
+  { name: "Recruit Macready", types: ["companion"] },
+  { name: "Eat the Happy Birthday sweet roll", types: ["items"] },
+  { name: "Cook any soup", types: ["crafting"] },
+  { name: "Obtain a Napkin", types: ["items"] },
+  { name: "Obtain Jangles the Moon Monkey", types: ["items"] },
+  { name: "Become a Pillar of the Community", types: ["strip"] }
+];
+bingoList[5] = [
+  { name: "Collect 5 Perk Magazines", types: ["items"] },
+  { name: "Collect at least 3 BobbleHeads", types: ["snow"] },
+  { name: "Obtain the Shem Drowne sword", types: ["weapon"] },
+  { name: "Kill Moe Cronin with a Baseball Bat", types: ["kill"] },
+  { name: "Create a Dirty Wastelander", types: ["crafting"] },
+  { name: "Obtain Big Jim", types: ["weapon"] },
+  { name: "Collect the Silver Shroud Costume", types: ["items"] },
+  { name: "Construct Mama Murphy's Chair", types: ["meme"] },
+  { name: "Collect a Burnt Comic", types: ["items"] },
+  { name: "Obtain a Teddy Bear", types: ["items"] }
+];
+bingoList[6] = [
+  { name: "Kill Ashes", types: ["kill"] },
+  { name: "Complete the quest Pull the Plug", types: ["quest"] },
+  { name: "Wear a different vault jumpsuit", types: ["items"] },
+  { name: "Bring a mutated fern flower to Solomon", types: ["quest"] },
+  { name: "Collect a Zeta gun", types: ["weapon"] },
+  { name: "Cook a Mirelurk Cake", types: ["crafting"] },
+  { name: "Kill someone using a Syringer", types: ["kill"] },
+  { name: "Pickpocket 5 seperate items", types: ["items"] },
+  { name: "Obtain a HalluciGen gas canister", types: ["items"] }
+];
+bingoList[7] = [
+  { name: "Have sex", types: ["sex"] },
+  { name: "Become a ghoul on the pipboy screen", types: ["perk"] },
+  { name: "Recruit Nick Valentine", types: ["companion"] },
+  { name: "Collect the 2076 World Series Baseball Bat", types: ["weapon"] },
+  { name: "Become addicted to at least 5 substances", types: ["items"] },
+  { name: "Drink a glowing blood pack", types: ["items"] },
+  { name: "Obtain Green Paint", types: ["items"] },
+  { name: "Obtain Grandpa Savoldi's hat", types: ["location"] }
+];
+bingoList[8] = [
+  { name: "Obtain the Home Plate", types: ["location"] },  
+  { name: "Acquire General Chao's Revenge", types: ["weapon"] }, 
+  { name: "Obtain Grognak's Axe", types: ["weapon"] },
+  { name: "Cook a Grilled RadStag", types: ["crafting"] },
+  { name: "Obtain a Mysterious Serum", types: ["items"] },
+  { name: "Obtain the Mascot Head", types: ["items"] }
+];
+bingoList[9] = [
+  { name: "Consume an Experimental Plant", types: ["game"] },
+  { name: "Kill any Mirelurk King", types: ["kill"] },
+  { name: "Collect the Reba and the Reba II", types: ["weapon"] },
+  { name: "Successfully reverse pickpocket a grenade onto someone", types: ["kill"] },
+  { name: "Get Good Intentions", types: ["weapon"] },
+  { name: "Complete the quest Human Error", types: ["quest"] }
+];
+bingoList[10] = [
+  { name: "Get mole rat cure", types: ["quest"] },
+  { name: "Become an Atom Cat", types: ["game"] },
+  { name: "Obtain the Junk Jet", types: ["weapon"] },
+  { name: "Obtain the Deliverer", types: ["weapon"] },
+  { name: "Recruit 5 companions", types: ["companion"] },
+  { name: "Reopen the General Atomics Galleria", types: ["location"] },
+  { name: "Complete the quest Curtain Call", types: ["quest"] }
+];
+bingoList[11] = [
+  { name: "Convince Miss Edna to get married", types: ["quest"] },
+  { name: "Defeat the Champ using boxing gloves in the Madden Gym", types: ["kill"] },
+  { name: "Complete the quest Pickman's Gift", types: ["quest"] },
+  { name: "Sell Billy into slavery", types: ["kill"] },
+  { name: "Kill a Sentry Bot", types: ["kill"] },
+  { name: "Collect 2 bottle messages", types: ["notes"] },
+  { name: "Equip something in the eye, headband, neck and torso slots on Dogmeat.", types: ["items"] }
+];
+bingoList[12] = [
+  { name: "Complete the quest Last Voyage of the U.S.S. Constitution", types: ["quest"] },
+  { name: "Complete the quest Diamond City Blues", types: ["quest"] },
+  { name: "Complete the quest Confidence Man", types: ["quest"] },
+  { name: "Obtain the Survivor's Special", types: ["weapons"] }
+];
+bingoList[13] = [
+  { name: "Obtain the Gainer", types: ["weapons"] },
+  { name: "Obtain Kremvh's Tooth", types: ["weapon"] },
+  { name: "Complete the quest The Big Dig", types: ["quest"] },
+  { name: "Kill a Mirelurk Queen", types: ["kill"] },
+  { name: "Kill Swan", types: ["kill"] }
+];
+bingoList[14] = [
+  { name: "Obtain the Cryolator", types: ["weapon"] },
+  { name: "Have at least 4000 caps", types: ["money"] },
+  { name: "Complete the quest The Silver Shroud", types: ["quest"] }
+];
+bingoList[15] = [
+  { name: "Collect a Nuclear Warhead", types: ["items"] },
+  { name: "Kill Kellogg", types: ["kill"] },
+  { name: "Become allied with 1 settlement", types: ["rep"] },
+  { name: "Cook a Slocum's Buzzbite", types: ["crafting"] },
+  { name: "Complete the quest The Cleaner", types: ["quest"] }
+];
+bingoList[16] = [
+  { name: "Overdose Mama Murphy", types: ["crafting"] },
+  { name: "Acquire the Rank of General of the Commonwealth Minutemen", types: ["location"] },
+  { name: "Discover all the Vaults", types: ["location"] },
+  { name: "Ride a Vertibird", types: ["quest"] },
+  { name: "Destroy a Vertibird", types: ["meme"] }
+];
+bingoList[17] = [
+  { name: "Complete the quest Cambridge Polymer Labs", types: ["quest"] },
+  { name: "Complete QA testing in the General Atomics Factory", types: ["quest"] },
+  { name: "Collect a Courser Chip", types: ["weapon"] },
+  { name: "Free K1-98", types: ["location"] },
+  { name: "Drop the Wedding Ring into molten metal at Saugus Ironworks", types: ["meme"] }
+];
+bingoList[18] = [
+  { name: "Kill 2 Behemoths", types: ["kill"] },
+  { name: "Eat Big Mack", types: ["meme"] },
+  { name: "Kill Wire with a Laser Musket", types: ["kill"] },
+  { name: "Collect at least 10 unique weapons", types: ["weapon"] },
+  { name: "Return the Pristine Deathclaw egg", types: ["quest"] }
+];
+bingoList[19] = [
+  { name: "Obtain the Piezonucleic power armor", types: ["items"] },
+  { name: "Set foot onto the Prydwen", types: ["location"] },
+  { name: "Collect 10 bobbleheads", types: ["snow"] },
+  { name: "Collect a Shishkebab", types: ["weapon"] }
+];
+bingoList[20] = [
+  { name: "Collect all holotape games", types: ["star"] },
+  { name: "Obtain the Buttercup toy from Arlen Glass", types: ["items"] },
+  { name: "Become allied with 2 settlements", types: ["rep"] }
+];
+bingoList[21] = [
+  { name: "Kill 3 Mirelurk Queens", types: ["kill"] },
+  { name: "Get banished from the Institute", types: ["quest"] },
+  { name: "Collect at least 15 Unique Weapons", types: ["weapon"] },
+  { name: "Collect at least 15 Bobbleheads", types: ["snow"] }
+];
+bingoList[22] = [
+  { name: "Get Lorenzo's Artifact gun", types: ["weapon"] },
+  { name: "Get Lorenzo's Suit", types: ["items"] },
+  { name: "Kill Father with a Baseball Grenade", types: ["meme"] },
+  { name: "Kill Father with Kellogg's Pistol", types: ["kill"] }
+];
+bingoList[23] = [
+  { name: "Complete the quest Emergent Behaviour", types: ["challenge"] },
+  { name: "Collect at least 20 Unique Weapons", types: ["weapon"] },
+  { name: "Drop Father's lab coat into Shaun's crib at Sanctuary", types: ["meme"] },
+  { name: "Collect at least 20 Bobbleheads", types: ["snow"] },
+  { name: "Discover at least 20 settlements", types: ["location"] }			
+];
+bingoList[24] = [
+  { name: "Take the full Vault 81 Tour", types: ["challenge"] },
+  { name: "Kill self", types: ["kill"] },
+  { name: "Relive your memories", types: ["meme"] },
+  { name: "Become allied with 3 settlements", types: ["rep"] }
+];
+bingoList[25] = [
+  { name: "Find all bottle messages", types: ["notes"] },
+  { name: "Collect 9 Eddie Winter holotapes", types: ["notes"] },
+  { name: "Complete the game", types: ["challenge"] }
+];
+	$(function() { srl.bingo(bingoList, 5); });
---
+++ Fallout 4 Bingo/readme.txt
@@ -0,0 +1,15 @@
+Fallout 4 Bingo Board version 1.0 by Kungkobra, Akarion, Snowcone Joey and Elongated Musketgun.
+
+1. open up f4bingo.htm in your favourite Netscape alternative
+2. generate a cool card that you like
+3. if you have any friends (haha yeah right) copy the seed number at the end of the URL and share it
+4. try to find the fastest row/column and complete it
+5. do with or without glitches, idc
+
+If you want more goals, open f4.js in the folder and add them accordingly.
+Earlier goals are easier/short ones, later ones are harder/longer ones, on a scale of 1 to 25.
+
+changes:
+
+1.0
+- Initial version.
